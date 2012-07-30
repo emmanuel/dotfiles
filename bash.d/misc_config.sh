3022712b5ed6a6c65f7172afebd4555dd58a1c37
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2012-07-30 01:07:09 -0700

    This is handled in bash.d/completion.sh.

--- bash.d/misc_config.sh
+++ bash.d/misc_config.sh
@@ -15,13 +15,3 @@ shopt -s cdable_vars
 # values of LINES and COLUMNS.
 shopt -s checkwinsize
 
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
-    . /etc/bash_completion
-fi
-
-
-# vim: ft=bash fdm=marker expandtab ts=3 sw=3 :
-
