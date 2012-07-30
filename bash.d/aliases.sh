e99978b3d3dfb5604a681f4a0ea026e6cd521993
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2012-07-30 01:05:54 -0700

    Remove several aliases and add two.

--- bash.d/aliases.sh
+++ bash.d/aliases.sh
@@ -12,8 +12,6 @@ alias gvivi='gvim $HOME/.vimrc'
 alias viil='vim $DSYSDATA/install.log -c "\$"'
 alias gviil='gvim $DSYSDATA/install.log -c "\$"'
 
-alias mountbooks='truecrypt -t -k "" --protect-hidden=no /media/PLATYPUS/c/secure.tc $DOTHER/books/'
-
 alias ..='cd ..'
 alias ..2='cd ../..'
 alias ..3='cd ../../..'
@@ -24,7 +22,6 @@ alias ..?='show_parent_dirs'
 alias du1='du --max-depth 1'
 alias dumd='du --max-depth'
 
-alias clearcache='sudo sync; sudo echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null'
-
-alias mc='mv' 
-alias cdp='. cdp.sh'
+alias lesstail='less +F'
+alias less='less -R'
+alias ll='ls -lha'
