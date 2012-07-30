aa8a24b96159f9cbb656790ee3bfca3317bece9d
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2012-07-30 00:37:06 -0700

    Use Homebrew bash completions instead of /etc.

--- bash.d/completion.sh
+++ bash.d/completion.sh
@@ -58,10 +58,6 @@ complete -C _complete_ansi_color -o default ansi_color
 # ---------------------------------------------------------------------------
 
 
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
-    . /etc/bash_completion
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+  . `brew --prefix`/etc/bash_completion
 fi
-
