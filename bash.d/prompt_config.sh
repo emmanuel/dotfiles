034f9ddecbac5db87a33ee5d694215c8114ded24
Emmanuel Gomez <emmanuel.gomez@nordstrom.com>
2013-08-08 00:21:01 -0700

    Colorize Bash prompt with Solarized colors.
    From: https://github.com/necolas/dotfiles

--- bash.d/prompt_config.sh
+++ bash.d/prompt_config.sh
@@ -1,83 +1,125 @@
-# Set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
-   debian_chroot=$(cat /etc/debian_chroot)
+#!/bin/bash
+
+# bash_prompt (mainly) from: https://github.com/necolas/dotfiles
+
+# Example:
+# nicolas@host: ~/.dotfiles on master[+!?$]
+# $
+
+# Screenshot: http://i.imgur.com/DSJ1G.png
+# iTerm2 prefs: import Solarized theme (disable bright colors for bold text)
+# Color ref: http://vim.wikia.com/wiki/Xterm256_color_names_for_console_Vim
+# More tips: http://www.cyberciti.biz/tips/howto-linux-unix-bash-shell-setup-prompt.html
+
+
+# Check that terminfo exists before changing TERM var to xterm-256color
+# Prevents prompt flashing in Mac OS X 10.6 Terminal.app
+if [ -e /usr/share/terminfo/x/xterm-256color ]; then
+    export TERM='xterm-256color'
 fi
 
-# Prompt Items:
-# 
-#   \u              Username of the current user
-#   \h              Hostname up to the first '.'
-#   \w              Current working directory
-#   $debian_chroot  Current chroot (if any)
-#   `__git_ps1`     Current git branch if any
-#
-PS1NOCOLOR='\[\033[0m\]${debian_chroot:+($debian_chroot)}\u@\h:\w`__git_ps1`\$ '
-PS1NOCOLOR='\u@\h:\w`__git_ps1`\$ '
-PS1="\[\033[0m\]$PS1NOCOLOR"
+tput sgr 0 0
 
-# # If this is an xterm set the title to user@host:dir
-# case "$TERM" in
-# xterm*|rxvt*)
-#     PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-#     ;;
-# *)
-#     ;;
-# esac
+# Base styles and color palette
+# Solarized colors
+# https://github.com/altercation/solarized/tree/master/iterm2-colors-solarized
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+SOLAR_YELLOW=$(tput setaf 136)
+SOLAR_ORANGE=$(tput setaf 166)
+SOLAR_RED=$(tput setaf 124)
+SOLAR_MAGENTA=$(tput setaf 125)
+SOLAR_VIOLET=$(tput setaf 61)
+SOLAR_BLUE=$(tput setaf 33)
+SOLAR_CYAN=$(tput setaf 37)
+SOLAR_GREEN=$(tput setaf 64)
+SOLAR_WHITE=$(tput setaf 254)
 
+style_user="\[${RESET}${SOLAR_ORANGE}\]"
+style_host="\[${RESET}${SOLAR_YELLOW}\]"
+style_path="\[${RESET}${SOLAR_GREEN}\]"
+style_chars="\[${RESET}${SOLAR_WHITE}\]"
+style_branch="${SOLAR_CYAN}"
 
+if [[ "$SSH_TTY" ]]; then
+    # connected via ssh
+    style_host="\[${BOLD}${SOLAR_RED}\]"
+elif [[ "$USER" == "root" ]]; then
+    # logged in as root
+    style_user="\[${BOLD}${SOLAR_RED}\]"
+fi
 
-ansi_color()
-{
-   local COLOR_BLACK="\033[0;30m"
-   local COLOR_RED="\033[0;31m"
-   local COLOR_GREEN="\033[0;32m"
-   local COLOR_BROWN="\033[0;33m"
-   local COLOR_BLUE="\033[0;34m"
-   local COLOR_PURPLE="\033[0;35m"
-   local COLOR_CYAN="\033[0;36m"
-   local COLOR_LIGHT_GRAY="\033[0;37m"
-   local COLOR_DARK_GRAY="\033[1;30m"
-   local COLOR_LIGHT_RED="\033[1;31m"
-   local COLOR_LIGHT_GREEN="\033[1;32m"
-   local COLOR_YELLOW="\033[1;33m"
-   local COLOR_LIGHT_BLUE="\033[1;34m"
-   local COLOR_LIGHT_PURPLE="\033[1;35m"
-   local COLOR_LIGHT_CYAN="\033[1;36m"
-   local COLOR_WHITE="\033[1;37m"
-   local COLOR_NONE="\033[m"
-   local color=$COLOR_NONE
-   case "$1" in
-      black)        color=$COLOR_BLACK;;
-      red)          color=$COLOR_RED;;
-      green)        color=$COLOR_GREEN;;
-      brown)        color=$COLOR_BROWN;;
-      blue)         color=$COLOR_BLUE;;
-      purple)       color=$COLOR_PURPLE;;
-      cyan)         color=$COLOR_CYAN;;
-      light_gray)   color=$COLOR_LIGHT_GRAY;;
-      dark_gray)    color=$COLOR_DARK_GRAY;;
-      light_red)    color=$COLOR_LIGHT_RED;;
-      light_green)  color=$COLOR_LIGHT_GREEN;;
-      yellow)       color=$COLOR_YELLOW;;
-      light_blue)   color=$COLOR_LIGHT_BLUE;;
-      light_purple) color=$COLOR_LIGHT_PURPLE;;
-      light_cyan)   color=$COLOR_LIGHT_CYAN;;
-      white)        color=$COLOR_WHITE;;
-      none)         color=$COLOR_NONE;;
-   esac
-   echo "$color"
+is_git_repo() {
+    $(git rev-parse --is-inside-work-tree &> /dev/null)
 }
 
-set_prompt_color()
-{
-   local colorname=${1:-none}
-   local promptcolor="\[$(ansi_color "$colorname")\]"
-   local nocolor="\[$(ansi_color none)\]"
-   PS1BAK="$PS1"
-   PS1="${promptcolor}${PS1NOCOLOR}${nocolor}"
-}  
-
-default_prompt_color()
-{
-   PS1='\[\033[0m\]$PS1NOCOLOR'
+is_git_dir() {
+    $(git rev-parse --is-inside-git-dir 2> /dev/null)
 }
+
+get_git_branch() {
+    local branch_name
+
+    # Get the short symbolic ref
+    branch_name=$(git symbolic-ref --quiet --short HEAD 2> /dev/null) ||
+    # If HEAD isn't a symbolic ref, get the short SHA
+    branch_name=$(git rev-parse --short HEAD 2> /dev/null) ||
+    # Otherwise, just give up
+    branch_name="(unknown)"
+
+    printf $branch_name
+}
+
+# Git status information
+prompt_git() {
+    local git_info git_state uncommitted unstaged untracked stashed
+
+    if ! is_git_repo || is_git_dir; then
+        return 1
+    fi
+
+    git_info=$(__git_ps1)
+
+    # Check for uncommitted changes in the index
+    if ! $(git diff --quiet --ignore-submodules --cached); then
+        uncommitted="+"
+    fi
+
+    # Check for unstaged changes
+    if ! $(git diff-files --quiet --ignore-submodules --); then
+        unstaged="!"
+    fi
+
+    # Check for untracked files
+    if [ -n "$(git ls-files --others --exclude-standard)" ]; then
+        untracked="?"
+    fi
+
+    # Check for stashed files
+    if $(git rev-parse --verify refs/stash &>/dev/null); then
+        stashed="$"
+    fi
+
+    git_state=$uncommitted$unstaged$untracked$stashed
+
+    # Combine the branch name and state information
+    if [[ $git_state ]]; then
+        git_info="$git_info[$git_state]"
+    fi
+
+    printf " ${style_branch}${git_info}"
+}
+
+
+# Set the terminal title to the current working directory
+PS1="\[\033]0;\w\007\]"
+# Build the prompt
+# PS1+="\n" # Newline
+PS1+="${style_user}\u" # Username
+PS1+="${style_chars}@" # @
+PS1+="${style_host}\h" # Host
+PS1+="${style_chars}: " # :
+PS1+="${style_path}\w" # Working directory
+PS1+="\$(prompt_git)" # Git details
+# PS1+="\n" # Newline
+PS1+="${style_chars}\$ \[${RESET}\]" # $ (and reset color)
