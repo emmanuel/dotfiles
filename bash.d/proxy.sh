0076ee756ef964d7b59fa89455ec1ad924118c1d
Emmanuel Gomez <emmanuel@gomez.io>
2017-01-22 11:13:33 -0800

    Add a more complete (and complex) `no_proxy` value.

--- bash.d/proxy.sh
+++ bash.d/proxy.sh
@@ -10,11 +10,12 @@ function proxyoff
 # Proxy
 function proxyon
 {
-    http_proxy=http://webproxysea.nordstrom.net:8181/
+    http_proxy=http://webproxysea.nordstrom.net:8181
     HTTP_PROXY=$http_proxy
     https_proxy=$http_proxy
     HTTPS_PROXY=$https_proxy
-    no_proxy=".nordstrom.net"
+    # no_proxy=".local,169.254/16,172.16/12,.nordstrom.net,norm.nordstrom.com,federation.nordstrom.com"
+    no_proxy="127.0.0.1,localhost,.local,169.254/16,.nordstrom.net,.nordstrom.net,norm.nordstrom.com,nordstrom.net,192.168.99.100,192.168.99.101,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,.dev.nordstrom.com,.wsperf.nordstrom.com,.website.nordstrom.com,.amazonaws.com"
     NO_PROXY=$no_proxy
     export http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY
     # git config --global http.proxy $http_proxy
