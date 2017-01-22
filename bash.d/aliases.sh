d8e3fe0d4346b0101398ff4e2f0efeb7e99f2869
Emmanuel Gomez <emmanuel@gomez.io>
2017-01-22 11:16:03 -0800

    Add `kc` and `kcs` aliases for `kubectl`.

--- bash.d/aliases.sh
+++ bash.d/aliases.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+alias kc=kubectl
+alias kcs='kubectl --namespace=kube-system'
+
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
 alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
 
