386ca5674dafbf166e9025ba0e037eb5781fedf4
Emmanuel Gomez <emmanuel@gomez.io>
2024-04-23 01:53:28 -0700

    feat: Migrate most things into subfolders.

--- Brewfile
+++ Brewfile
@@ -3,18 +3,16 @@ brew 'brew-cask-completion'
 brew 'git'
 brew 'gpg'
 
-brew 'go'
 brew 'nodejs' # includes 'npm'
+brew 'go'
 brew 'rustup'
-brew 'yarn'
-brew 'elm'
-# brew 'elm-format'
 
 brew 'watch'
 brew 'tree'
 brew 'jq'
 brew 'direnv'
 
+brew 'aws-sso-cli'
 brew 'hub'
 brew 'kubernetes-cli'
 brew 'awscli'
@@ -22,40 +20,38 @@ brew 'terraform'
 brew 'openconnect'
 brew 'bitwarden-cli'
 
+cask '1password'
 cask 'launchbar'
 cask 'iterm2'
-cask 'gitup'
 cask 'zed'
 cask 'visual-studio-code'
-cask 'intellij-idea-ce'
+cask 'intellij-idea'
+cask 'gitup'
 
 cask 'docker'
 cask 'postgres-unofficial'
 cask 'postico'
 
-cask 'xbar'
 cask 'thingsmacsandboxhelper'
-cask 'aws-vault'
 
-cask 'finicky'
-cask 'browserosaurus'
 cask 'google-chrome'
 cask 'firefox'
 
-
-cask 'keybase'
-# cask 'discord'
+cask 'discord'
 cask 'zoom'
-cask 'spotify'
+cask 'webex'
 
+cask 'spotify'
+cask 'steam'
 cask 'vlc'
 cask 'transmission'
 
 brew 'mas'
 
-mas 'Moom',             id: 419330170
-mas 'Bitwarden',        id: 1352778147
-mas 'The Unarchiver',   id: 425424353
-mas 'Slack',            id: 803453959
-mas 'Things',           id: 904280696
-mas 'Xcode',            id: 497799835
+mas 'Moom',                 id: 419330170
+mas 'Bitwarden',            id: 1352778147
+mas 'The Unarchiver',       id: 425424353
+mas 'Slack',                id: 803453959
+mas 'Things',               id: 904280696
+mas 'Xcode',                id: 497799835
+mas '1Password for Safari', id: 1569813296
--- finicky.js
+++ finicky.js
@@ -1,23 +1,24 @@
 module.exports = {
   defaultBrowser: "Browserosaurus",
-  handlers: [{
+  handlers: [
+    {
       match: /^https?:\/\/meet\.google\.com\/.*$/,
-      browser: "Google Chrome"
+      browser: "Google Chrome",
     },
     // https://github.com/johnste/finicky/wiki/Configuration-ideas#open-spotify-links-in-spotify-app
     {
       match: "open.spotify.com/*",
-      browser: "Spotify"
+      browser: "Spotify",
     },
     // https://github.com/johnste/finicky/wiki/Configuration-ideas#open-zoom-links-in-zoom-app
-    {
-      match: [
-        "zoom.us/*",
-        finicky.matchDomains(/.*\zoom.us/),
-        /zoom.us\/j\//,
-      ],
-      browser: "us.zoom.xos"
-    },
+    // {
+    //   match: [
+    //     "zoom.us/*",
+    //     finicky.matchDomains(/.*\zoom.us/),
+    //     /zoom.us\/j\//,
+    //   ],
+    //   browser: "us.zoom.xos"
+    // },
     // {
     //   match: [
     //     "*.webex.com/*",
@@ -29,24 +30,31 @@ module.exports = {
     // },
   ],
   // https://github.com/johnste/finicky/wiki/Configuration-ideas#remove-all-marketingtracking-information-from-urls
-  rewrite: [{
-    match: () => true, // Execute rewrite on all incoming urls to make this example easier to understand
-    url({
-      url
-    }) {
-      const removeKeysStartingWith = ["utm_", "uta_"]; // Remove all query parameters beginning with these strings
-      const removeKeys = ["fblid", "gclid"]; // Remove all query parameters matching these keys
+  rewrite: [
+    {
+      match: () => true, // Execute rewrite on all incoming urls to make this example easier to understand
+      url({ url }) {
+        const removeKeysStartingWith = ["utm_", "uta_"]; // Remove all query parameters beginning with these strings
+        const removeKeys = ["fblid", "gclid"]; // Remove all query parameters matching these keys
 
-      const search = url.search
-        .split("&")
-        .map((parameter) => parameter.split("="))
-        .filter(([key]) => !removeKeysStartingWith.some((startingWith) => key.startsWith(startingWith)))
-        .filter(([key]) => !removeKeys.some((removeKey) => key === removeKey));
+        const search = url.search
+          .split("&")
+          .map((parameter) => parameter.split("="))
+          .filter(
+            ([key]) =>
+              !removeKeysStartingWith.some((startingWith) =>
+                key.startsWith(startingWith),
+              ),
+          )
+          .filter(
+            ([key]) => !removeKeys.some((removeKey) => key === removeKey),
+          );
 
-      return {
-        ...url,
-        search: search.map((parameter) => parameter.join("=")).join("&"),
-      };
+        return {
+          ...url,
+          search: search.map((parameter) => parameter.join("=")).join("&"),
+        };
+      },
     },
-  }],
-}
+  ],
+};
